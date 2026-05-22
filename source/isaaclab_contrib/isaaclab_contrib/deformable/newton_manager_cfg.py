0f657cd99c4829bd3e2f96fddebf5de3b92d618a
Mike Yan Michelis <mmichelis@nvidia.com>
2026-05-22 16:38:10 +0200

    Style: Make it clearer graph coloring cannot be set by user

--- source/isaaclab_contrib/isaaclab_contrib/deformable/newton_manager_cfg.py
+++ source/isaaclab_contrib/isaaclab_contrib/deformable/newton_manager_cfg.py
@@ -7,7 +7,7 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, ClassVar
 
 from isaaclab_newton.physics import FeatherstoneSolverCfg, MJWarpSolverCfg, NewtonCfg, NewtonSolverCfg
 
@@ -33,7 +33,7 @@ class VBDSolverCfg(NewtonSolverCfg):
 
     requires_graph_coloring: bool = True
 
-    requires_graph_coloring: bool = True
+    requires_graph_coloring: ClassVar[bool] = True
 
     iterations: int = 10
     """Number of VBD iterations per substep."""
@@ -114,7 +114,7 @@ class CoupledMJWarpVBDSolverCfg(NewtonSolverCfg):
     class_type: type[NewtonManager] | str = "{DIR}.coupled_mjwarp_vbd_manager:NewtonCoupledMJWarpVBDManager"
     """Manager class for the coupled MJWarp + VBD solver."""
 
-    requires_graph_coloring: bool = True
+    requires_graph_coloring: ClassVar[bool] = True
 
     rigid_solver_cfg: MJWarpSolverCfg = MJWarpSolverCfg()
     """Rigid-body sub-solver configuration."""
@@ -207,7 +207,7 @@ class CoupledFeatherstoneVBDSolverCfg(NewtonSolverCfg):
     class_type: type[NewtonManager] | str = "{DIR}.coupled_featherstone_vbd_manager:NewtonCoupledFeatherstoneVBDManager"
     """Manager class for the coupled Featherstone + VBD solver."""
 
-    requires_graph_coloring: bool = True
+    requires_graph_coloring: ClassVar[bool] = True
 
     rigid_solver_cfg: FeatherstoneSolverCfg = FeatherstoneSolverCfg()
     """Rigid-body sub-solver configuration."""
