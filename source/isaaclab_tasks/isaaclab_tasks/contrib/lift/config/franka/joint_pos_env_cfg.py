e22ebcc041db2b0a5fcec70f5c97c7769025c56b
Mike Yan Michelis <mmichelis@nvidia.com>
2026-07-22 20:23:27 +0200

    Test: stable training params

--- source/isaaclab_tasks/isaaclab_tasks/contrib/lift/config/franka/joint_pos_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/contrib/lift/config/franka/joint_pos_env_cfg.py
@@ -103,7 +103,7 @@ class FrankaCubeLiftEnvCfg(LiftEnvCfg):
 
         # Set actions for the specific robot type (franka)
         self.actions.arm_action = mdp.RelativeJointPositionActionCfg(
-            asset_name="robot", joint_names=["panda_joint.*"], scale=0.04
+            asset_name="robot", joint_names=["panda_joint.*"], scale=0.02
         )
         self.actions.gripper_action = mdp.JointPositionToLimitsActionCfg(
             asset_name="robot", joint_names=["panda_finger.*"], rescale_to_limits=True
