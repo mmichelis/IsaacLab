24ee8f0e0ad6e4bb1c1e01fe15b52f3f9e4731c6
Mike Yan Michelis <mmichelis@nvidia.com>
2026-07-30 18:34:08 +0200

    Test: Up the max iteration count

--- source/isaaclab_tasks/isaaclab_tasks/core/lift/config/franka_soft/agents/rsl_rl_ppo_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/core/lift/config/franka_soft/agents/rsl_rl_ppo_cfg.py
@@ -15,7 +15,7 @@ from isaaclab_rl.rsl_rl import (
 @configclass
 class FrankaSoftLiftPPORunnerCfg(RslRlOnPolicyRunnerCfg):
     num_steps_per_env = 24
-    max_iterations = 2000
+    max_iterations = 5000
     save_interval = 50
     experiment_name = "franka_soft"
     obs_groups = {
